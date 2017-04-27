2f11bacc60c1ba02ac330a599d0081abbe3b9390
cpcx <cpcx@163.com>
2017-04-27 17:01:09 +0800

    添加多环境配置(包括日志，数据源)

--- src/main/java/com/jk/controller/GlobalErrorController.java
+++ src/main/java/com/jk/controller/GlobalErrorController.java
@@ -1,5 +1,7 @@
 package com.jk.controller;
 
+import com.xiaoleilu.hutool.log.Log;
+import com.xiaoleilu.hutool.log.LogFactory;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class GlobalErrorController implements ErrorController {
 
+    private static final Log LOG = LogFactory.get();
+
     private static final String ERROR_PATH = "/error";
 
     @RequestMapping(value=ERROR_PATH)
     public String handleError(){
+        LOG.error("访问的页面不存在！");
         return "admin/common/404";
     }
 
