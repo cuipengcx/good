22aa399d9e4bf6f9412c47287c7d2bfb86f49599
cpcx <cpcx@163.com>
2017-05-04 17:12:57 +0800

    添加quartz配置

--- src/main/java/com/jk/service/ScheduleJobService.java
+++ src/main/java/com/jk/service/ScheduleJobService.java
@@ -2,9 +2,57 @@ package com.jk.service;
 
 import com.jk.model.ScheduleJob;
 
+import java.util.List;
+
 /**
  * @author cuiP
  * Created by JK on 2017/5/4.
  */
 public interface ScheduleJobService extends BaseService<ScheduleJob>{
+
+    /**
+     * 根据ID，查询定时任务
+     */
+    ScheduleJob findScheduleJobById(Long jobId);
+
+    /**
+     * 查询定时任务列表
+     */
+    List<ScheduleJob> findPage();
+
+    /**
+     * 保存定时任务
+     */
+    void saveScheduleJob(ScheduleJob scheduleJob);
+
+    /**
+     * 更新定时任务
+     */
+    void updateScheduleJob(ScheduleJob scheduleJob);
+
+    /**
+     * 批量删除定时任务
+     */
+    void deleteBatchScheduleJob(Long[] jobIds);
+
+    /**
+     * 批量更新定时任务状态
+     */
+    int updateBatchScheduleJob(Long[] jobIds, int status);
+
+    /**
+     * 立即执行
+     */
+    void run(Long[] jobIds);
+
+    /**
+     * 暂停运行
+     */
+    void pause(Long[] jobIds);
+
+    /**
+     * 恢复运行
+     */
+    void resume(Long[] jobIds);
+    
 }
--- src/main/java/com/jk/service/impl/ScheduleJobServiceImpl.java
+++ src/main/java/com/jk/service/impl/ScheduleJobServiceImpl.java
@@ -5,6 +5,8 @@ import com.jk.service.ScheduleJobService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 /**
  * @author cuiP
  * Created by JK on 2017/5/4.
@@ -12,4 +14,48 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Service
 public class ScheduleJobServiceImpl extends BaseServiceImpl<ScheduleJob> implements ScheduleJobService{
+    @Override
+    public ScheduleJob findScheduleJobById(Long jobId) {
+        return null;
+    }
+
+    @Override
+    public List<ScheduleJob> findPage() {
+        return null;
+    }
+
+    @Override
+    public void saveScheduleJob(ScheduleJob scheduleJob) {
+
+    }
+
+    @Override
+    public void updateScheduleJob(ScheduleJob scheduleJob) {
+
+    }
+
+    @Override
+    public void deleteBatchScheduleJob(Long[] jobIds) {
+
+    }
+
+    @Override
+    public int updateBatchScheduleJob(Long[] jobIds, int status) {
+        return 0;
+    }
+
+    @Override
+    public void run(Long[] jobIds) {
+
+    }
+
+    @Override
+    public void pause(Long[] jobIds) {
+
+    }
+
+    @Override
+    public void resume(Long[] jobIds) {
+
+    }
 }
