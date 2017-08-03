27a65b927b7ca5c39225277f21800ffe949e3e64
cpcx <cpcx@163.com>
2017-08-03 19:45:21 +0800

    封装webuploader上传组件

--- src/main/java/com/jk/modules/sys/controller/FileUploadController.java
+++ src/main/java/com/jk/modules/sys/controller/FileUploadController.java
@@ -1,7 +1,6 @@
 package com.jk.modules.sys.controller;
 
 import com.jk.common.base.controller.BaseController;
-import com.xiaoleilu.hutool.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.IOException;
 import java.util.UUID;
 
 /**
@@ -25,7 +25,7 @@ import java.util.UUID;
 @RequestMapping(value="/upload")
 public class FileUploadController extends BaseController {
 
-    private static final String FILE_UPLOAD_PATH = "/jkfa/attachment";
+    private static final String FILE_UPLOAD_PATH = "/goodmanage/attachment";
     
     private static final long MAX_IMG_SIZE = 1048576;
 
@@ -41,58 +41,35 @@ public class FileUploadController extends BaseController {
     @ResponseBody
     @PostMapping(value="/images")
     public String uploadImage(HttpServletRequest request, @RequestParam("file") MultipartFile file){
-        System.out.println(file.getOriginalFilename());
-        System.out.println(file.getName());
-        return fileupload(file, request).toString();
-    }
 
-    
-    private JSONObject fileupload(MultipartFile file, HttpServletRequest request){
         //使用 uuid 作为filename
         UUID uuid = UUID.randomUUID();
         String name= uuid.toString();
-        
+
         //生成文件的绝对路径
         String absTempPath = new File(FILE_UPLOAD_PATH).getAbsolutePath();
-        
+
         //上传文件的文件名
         String filename = file.getOriginalFilename();
-        
+
         //上传文件的扩展名
         String fileExt = filename.substring(filename.lastIndexOf(".")+1);
 
         //上传完毕后文件访问路径uri
         String serverurl = request.getRequestURL().substring(0,request.getRequestURL().indexOf(request.getRequestURI()));
-        
-        JSONObject obj = new JSONObject();
-        
-        if (!file.isEmpty()) {  
-            try {  
 
-                //超过1M，字节数超过 1048576
-//                if(file.getSize() > MAX_IMG_SIZE){
-//                    throw new Exception("图片超过1M");
-//                }
-                
-                File dest = new File(absTempPath + "/" + name + "." + fileExt); 
-                
-                file.transferTo(dest);
-                
-                //以json形式返回url，在富文本中显示
-                obj.put("error", 0);
-                obj.put("url", serverurl + "/upload/images?filename=" + name + "." + fileExt);
-                obj.put("dataurl", "/upload/images?filename=" + name + "." + fileExt);
-                
-                return obj;
-            } catch (Exception e) {  
-                obj.put("error",1);
-                obj.put("message","图片文件上传出错或上传图片超过1M");
-                return obj;
-            }  
-        } else {  
-            obj.put("error",1);
-            obj.put("message","上传图片文件为空");
-            return obj;
-        }  
+        if (!file.isEmpty()) {
+
+                File dest = new File(absTempPath + "/" + name + "." + fileExt);
+
+                try {
+                    file.transferTo(dest);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return serverurl;
+        }
+//        return fileupload(file, request).toString();
+        return "";
     }
 }
--- src/main/resources/static/js/common/web-upload-object.js
+++ src/main/resources/static/js/common/web-upload-object.js
@@ -1,23 +1,24 @@
 /**
- * web-upload 工具类
- * 
- * 约定：
- * 上传按钮的id = 图片隐藏域id + 'BtnId'
- * 图片预览框的id = 图片隐藏域id + 'PreId'
- * 
- * @author fengshuonan
+ * 对百度WebUploader的封装
+ * 因为这里考虑到每个需求可能不一样，比如文件大小限制，文件格式限制，宽高等，还是没法做成共通的插件
+ * 所以这些基本参数需要调用者自己定义，这里对回调进行了封装，调用者无需关心处理回调
  */
 (function() {
 	
-	var $WebUpload = function(pictureId) {
-		this.pictureId = pictureId;
-		this.uploadBtnId = pictureId + "BtnId";
-		this.uploadPreId = pictureId + "PreId";
-		this.uploadUrl = '/upload/images';
-		this.fileSizeLimit = 100 * 1024 * 1024;
-		this.picWidth = 800;
-		this.picHeight = 800;
-        this.uploadBarId = null;
+	var $WebUpload = function(options) {
+		this.auto = true;  //开启自动上传
+		this.uploadBtnId = options.uploadBtnId; //选择图片按钮，id|class自己定义
+		this.uploadPreId = options.picturePreId; //图片预览ID
+		this.uploadUrl = options.serverUrl;    //上传URL
+		this.swf = '/h-ui/lib/webuploader/0.1.5/Uploader.swf';
+		this.pictureId = options.hiddenPictureId;   //隐藏域ID,用于往后台传值
+		this.picWidth = options.width;  //图片宽度
+		this.picHeight = options.height; //图片高度
+		this.fileSizeLimit = options.fileSizeLimit;   //验证文件总大小是否超出限制, 超出则不允许加入队列
+		this.fileSingleSizeLimit = options.fileSingleSizeLimit;   // 验证单个文件大小是否超出限制, 超出则不允许加入队列
+		this.fileNumLimit = options.fileNumLimit;   // 验证文件总数量, 超出则不允许加入队列
+		this.accept = options.accept;  //指定接受哪些类型的文件
+        this.uploadBarId = null;      //图片上传的进度条的id
 	};
 
 	$WebUpload.prototype = {
@@ -34,40 +35,44 @@
 		 * 创建webuploader对象
 		 */
 		create : function() {
-			var webUploader = WebUploader.create({
-				auto : true,
+			return WebUploader.create({
+				auto : this.auto,
 				pick : {
-					id : '#' + this.uploadBtnId,
-					multiple : false // 只上传一个
+					id : this.uploadBtnId
+					// multiple : false // 只上传一个
 				},
-				accept : {
-					title : 'Images',
-					extensions : 'gif,jpg,jpeg,bmp,png',
-                    mimeTypes : 'image/gif,image/jpg,image/jpeg,image/bmp,image/png'
-				},
-				swf : '/h-ui/lib/webuploader/0.1.5/Uploader.swf',
+				accept : this.accept,
+				swf : this.swf,
 				disableGlobalDnd : true,
 				duplicate : true,
 				server : this.uploadUrl,
-				fileSingleSizeLimit : this.fileSizeLimit
+				fileSizeLimit : this.fileSizeLimit,
+				fileSingleSizeLimit : this.fileSingleSizeLimit,
+				fileNumLimit : this.fileNumLimit
 			});
-			
-			return webUploader;
 		},
 
 		/**
 		 * 绑定事件
 		 */
-		bindEvent : function(bindedObj) {
+		bindEvent : function(bindObj) {
 			var me =  this;
-			bindedObj.on('fileQueued', function(file) {
-				var $li = $('<div><img width="100px" height="100px"></div>');
+
+			//当文件被加入队列以后触发
+			bindObj.on('fileQueued', function(file) {
+				var $li = $(
+						'<div id="' + file.id + '" class="file-item thumbnail">' +
+						'<img>' +
+						'<div class="info">' + file.name + '</div>' +
+						'</div>'
+					);
+				// var $li = $('<div><img width="100px" height="100px"></div>');
 				var $img = $li.find('img');
 
 				$("#" + me.uploadPreId).html($li);
 
 				// 生成缩略图
-				bindedObj.makeThumb(file, function(error, src) {
+				bindObj.makeThumb(file, function(error, src) {
 					if (error) {
 						$img.replaceWith('<span>不能预览</span>');
 						return;
@@ -77,24 +82,52 @@
 			});
 
 			// 文件上传过程中创建进度条实时显示。
-			bindedObj.on('uploadProgress', function(file, percentage) {
-                $("#"+me.uploadBarId).css("width",percentage * 100 + "%");
+			bindObj.on('uploadProgress', function(file, percentage) {
+				console.log(percentage);
+                // $("#"+me.uploadBarId).css("width",percentage * 100 + "%");
+				var $li = $( '#'+file.id ),
+					$percent = $li.find('.progress span');
+
+				// 避免重复创建
+				if ( !$percent.length ) {
+					$percent = $('<p class="progress"><span></span></p>')
+						.appendTo( $li )
+						.find('span');
+				}
+
+				$percent.css( 'width', percentage * 100 + '%' );
 			});
 
 			// 文件上传成功，给item添加成功class, 用样式标记上传成功。
-			bindedObj.on('uploadSuccess', function(file,response) {
+			bindObj.on('uploadSuccess', function(file,response) {
 				succeedMessage("上传成功");
+				//隐藏域ID,用于往后台传值
 				$("#" + me.pictureId).val(response);
 				$('#' + file.id).addClass('upload-state-done');
 			});
 
+			// 完成上传完了，成功或者失败，先删除进度条。
+			bindObj.on('uploadComplete', function(file) {
+				$( '#'+file.id ).find('.progress').remove();
+			});
+
+
 			// 文件上传失败，显示上传出错。
-			bindedObj.on('uploadError', function(file) {
+			bindObj.on('uploadError', function(file) {
 				errorMessage("上传失败");
+				var $li = $( '#'+file.id ),
+					$error = $li.find('div.error');
+
+				// 避免重复创建
+				if ( !$error.length ) {
+					$error = $('<div class="error"></div>').appendTo( $li );
+				}
+
+				$error.text('上传失败');
 			});
 
 			// 其他错误
-			bindedObj.on('error', function(type) {
+			bindObj.on('error', function(type) {
 				if ("Q_EXCEED_SIZE_LIMIT" == type) {
 					errorMessage("文件大小超出了限制");
 				} else if ("Q_TYPE_DENIED" == type) {
@@ -108,9 +141,6 @@
 				}
 			});
 
-			// 完成上传完了，成功或者失败
-			bindedObj.on('uploadComplete', function(file) {
-			});
 		},
 
         /**
