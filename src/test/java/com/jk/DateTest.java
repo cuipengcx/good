9c5b98ba57af61a0d83a9438a24fd86f49122866
cpcx <228727120@qq.com>
2018-02-05 12:02:17 +0800

    新增lambda测试

--- src/test/java/com/jk/DateTest.java
+++ src/test/java/com/jk/DateTest.java
@@ -4,6 +4,9 @@ import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Test;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by JK on 2017/1/20.
@@ -14,5 +17,33 @@ public class DateTest {
     public void parseDate() throws ParseException {
         String dateTxt = "2017-1-1";
         System.out.println(DateUtils.parseDate(dateTxt,new String[]{"yyyy-MM-dd"}));
+    }
+
+    /**
+     * lambda对list的筛选过滤
+     */
+    @Test
+    public void filterListTest(){
+        List<Long> subDeptList = new ArrayList<>();
+        subDeptList.add(1L);
+        subDeptList.add(2L);
+        subDeptList.add(3L);
+
+        List<Long> ids = subDeptList.stream().map(id -> id).collect(Collectors.toList());
+        System.out.println(ids.toString());
+    }
+
+    /**
+     * lambda对list的复制
+     */
+    @Test
+    public void copyListTest(){
+        List<Long> subDeptList = new ArrayList<>();
+        subDeptList.add(1L);
+        subDeptList.add(2L);
+        subDeptList.add(3L);
+
+        List<Long> ids = subDeptList.stream().filter(id -> id < 3).collect(Collectors.toList());
+        System.out.println(ids.toString());
     }
 }
