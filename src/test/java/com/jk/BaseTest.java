a1ce10659127af9b4deb8f65f445ca226927d7cc
cpcx <228727120@qq.com>
2018-02-09 12:52:32 +0800

    单元测试

--- src/test/java/com/jk/BaseTest.java
+++ src/test/java/com/jk/BaseTest.java
@@ -4,14 +4,14 @@ import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  *
  * Created by JK on 2017/1/17.
  */
 @SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 public abstract class BaseTest {
     protected final transient Log log = LogFactory.get(this.getClass());
 }
